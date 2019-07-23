bf5a48b11daa759f955ad488716a3423b7f8f144
YonJu <yonlee13@gmail.com>
2019-07-23 21:15:09 +0900

    Initial commit

---
+++ src/Guest.java
@@ -0,0 +1,121 @@
+import java.io.*;
+import java.net.Socket;
+
+
+//클라이언트와의 데이터 송수신을 처리하는 서버측 클래스
+class Guest extends Thread {
+
+    String id;
+    String username;
+    String roomName;
+    Server server;
+    Socket socket;
+    BufferedReader reader;
+    BufferedWriter writer;
+
+
+    Guest(Server server, Socket socket) throws Exception {
+
+        this.server = server;
+        this.socket = socket;
+
+        InputStream is = socket.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        reader = new BufferedReader(isr);
+
+        OutputStream os = socket.getOutputStream();
+        OutputStreamWriter osw = new OutputStreamWriter(os);
+        writer = new BufferedWriter(osw);
+    }
+
+
+    public void run() {
+        try {
+            while (true) {
+
+                String line = reader.readLine();
+                System.out.println("message from client: "+line);
+                String array[] = line.split("/");
+
+                switch (array[0]) {
+
+                    //enter/id/username
+                    case "connect":
+                        id = array[1]; //사용자 id
+                        username = array[2]; //사용자 username
+                        roomName = array[3];
+
+                        server.consoleLog(id+" is connected");
+
+                        //이 사용자의 id, 닉네임, socket을 저장한다
+                        // => Server 클래스에서 addGuest()메소드로 이 guest를 목록에 추가함
+
+                        //해당 이름의 방이 있는지 확인하고, 방을 만들거나 입장시킨다
+                        server.checkIfRoomExists(roomName,this);
+
+                        break;
+
+                    //client.sendMsg("msg/메시지);
+                    case "msg":
+                        try {
+                            String message = array[1];
+
+                            //본인이 보낸 메시지를 전달하기 때문에, id를 붙일 필요 없다
+                            sendMsg("myMsg/"+message);
+                            //누가 보냈는지 알아야 하므로, 사용자의 id와 닉네임을 붙여서 보낸다
+                            server.broadcastToRoomExceptMe(roomName, "msg/"+id+"/"+username+"/"+message, id);
+                        }catch(Exception e){
+                            System.out.println("msg error: (no room?!)"+e);
+                        }
+                        break;
+
+
+                    case "closeRoom": //사용자가 방을 닫았을 때(inactive 상태)
+                        //네트워크 문제로 소켓이 끊긴 상태일 때도 closeRoom 으로 간주한다
+
+                        System.out.println(username+" is now inactive at room ("+roomName+")");
+                        server.broadcastToRoomExceptMe(roomName, "inactive/"+username+" is not reading messages.", id);
+
+                        break;
+//                    case "disconnect": //사용자가 방에서 나갔을 때
+//
+//                        //해당 사용자를 방 목록 / 전체 사용자 목록에서 제거한다
+//                        server.removeGuestFromRoom(roomName, this);
+//                        server.removeGuestFromLobby(this);
+//
+//                        break;
+
+//                    case "roomout" :
+//                        server.removeGuestFromRoom(array[2], this);
+//                        server.removeRoom(array[2]); //방에 아무도 없으면 방을 삭제
+//                        server.broadcastGuestlistAtLobby();
+//                        break;
+                }
+
+            }
+        } catch (Exception e) {
+            System.out.println("readLine() error:");
+            e.printStackTrace();
+
+            try {
+                //소켓이 끊기면, readline 시 null이 뜬다 -> NullPointerException 발생
+                //-> 해당 사용자를 inactive 상태로 간주한다
+                server.broadcastToRoomExceptMe(roomName, "inactive/"+username+" is not reading messages.", id);
+
+//                server.removeGuestFromRoom(roomName, this);
+//                server.removeGuestFromLobby(this);
+            } catch (Exception e1) {
+                System.out.println("removeGuest() error:"+ e1);
+            }
+        }
+    }
+
+    //이 사용자에게 메시지 보내기
+    public void sendMsg(String msg) throws Exception {
+
+        writer.write(msg + "\n");
+        writer.flush();
+    }
+
+}
+
---
+++ src/Server.java
@@ -0,0 +1,296 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.net.ServerSocket;
+
+import java.net.Socket;
+
+import java.util.ArrayList;
+
+import java.util.HashMap;
+
+import java.util.Set;
+
+
+//채팅방을 관리하는 서버측 클래스
+public class Server {
+
+    public static final int ServerPort = 8000;
+
+    ArrayList<Guest> listOfGuestsAtLobby; //대기실에 있는 손님 목록
+
+    HashMap<String, ArrayList<Guest>> roomHashMap; //방 목록 맵(방이름, 방에 속한 클라이언트 목록)
+
+
+    void initNet() throws Exception {
+
+        roomHashMap = new HashMap<String, ArrayList<Guest>>();
+
+//        listOfGuestsAtLobby = new ArrayList<Guest>();
+
+        //서버소켓 생성. 클라이언트 접속을 기다림
+        ServerSocket serverSocket = new ServerSocket(ServerPort);
+        consoleLog("Listening to 8000..");
+
+        while (true) {
+
+            Socket socket = serverSocket.accept();
+
+            consoleLog(socket.getInetAddress()+"is connected");
+
+            //Guest 클래스: 클라이언트와의 데이터 송수신을 처리하는 서버측 클래스(보기 편하라고 나눔)
+            //생성자 1 (Server): 서버의 메소드를 사용하기 위해 필요
+            //생성자 2 (socket): 클라이언트와의 데이터 송수신을 위해 필요
+            Guest guestThread = new Guest(this, socket);
+
+            guestThread.start();
+//            addGuest(guestThread); //새로운 게스트 객체를 로비 사용자 리스트에 추가한다
+        }
+
+    }
+
+    ///////////////////////////////////////
+
+
+    void checkIfRoomExists(String roomName, Guest guest) throws Exception{
+
+        if(roomHashMap.get(roomName) == null){ //이런 이름의 방이 존재하지 않는다면
+
+            consoleLog(roomName +" does not exist. Create one.");
+
+            addRoom(roomName, guest); //방을 새로 생성한다
+            guest.sendMsg("serverMsg/A room is created."); //입장 메시지 발송
+            broadcastRoomInfo(roomName); //방 정보 업데이트
+
+        }else{ //이미 해당 이름의 방이 있다면
+
+            consoleLog(roomName +" already exists.");
+
+            //이미 참여중인 방인지 확인
+            boolean isAlreadyJoinedRoom = false;
+            int index;
+
+            ArrayList<Guest> listOfGuestsInTheRoom = roomHashMap.get(roomName);
+            for(int i=0; i<listOfGuestsInTheRoom.size(); i++){
+                Guest anonymous_guest = listOfGuestsInTheRoom.get(i);
+                if(anonymous_guest.id.equals(guest.id)){
+                    isAlreadyJoinedRoom = true;
+
+                    anonymous_guest.socket = guest.socket;
+                    anonymous_guest.reader = guest.reader;
+                    anonymous_guest.writer = guest.writer;
+                }
+            }
+
+            consoleLog("isAlreadyJoinedRoom = "+isAlreadyJoinedRoom);
+
+            if(isAlreadyJoinedRoom){
+
+                consoleLog(guest.username+" has already joined "+roomName);
+
+                broadcastToRoomExceptMe(roomName, "return/"+guest.username+" is back.", guest.id); //다른 사람에게 보내는 메시지
+                guest.sendMsg("myReturn/Welcome back.");//나에게 보내는 메시지
+                broadcastRoomInfo(roomName);
+
+            }else{ //새로 가입한 방이라면
+
+                consoleLog(guest.username+" is new to "+roomName);
+
+                //join/welcome(나) --- join/oo entered the room(다른사람)
+                addGuestToRoom(roomName, guest); //기존 방에 해당 유저를 추가한다
+                broadcastToRoomExceptMe(roomName, "join/"+guest.username+" entered the room.", guest.id); //다른 사람에게 보내는 입장 메시지
+                guest.sendMsg("myJoin/Welcome."); //나에게 보내는 입장 메시지
+                broadcastRoomInfo(roomName); //방 정보 업데이트
+            }
+
+        }
+    }
+
+
+
+    //대기실 손님 목록에 해당 사용자 객체를 추가한다
+//    void addGuest(Guest guest) {
+//
+//        listOfGuestsAtLobby.add(guest);
+//        consoleLog("Number of Guests at Lobby : " + listOfGuestsAtLobby.size());
+//    }
+
+
+
+    //방목록에서 해당 방을 제거
+    void removeRoom(String roomName){
+
+        if(roomHashMap.get(roomName).size()==0){ //방에 아무도 없는 것을 확인하고
+
+            //해당 방을 방 목록(hashMap)에서 삭제한다
+            roomHashMap.remove(roomName);
+        }
+
+    }
+
+
+    //사용자가 방에서 나갔을 때, 후처리하는 메소드
+    void removeGuestFromRoom(String roomName, Guest guest) throws Exception{
+
+        //방 이름(key)으로 guest List를 불러온다. 이 list에서 해당 guest를 삭제한다
+        //value 로 삭제가 되나?? 된다. object를 삭제할 경우, remove(object) 하면 해당 object가 삭제된다
+        //이 경우 Guest 객체를 목록에서 삭제하는 것이다
+        //integer 를 삭제할 경우, remove((Integer) number) 라고 해야함
+        roomHashMap.get(roomName).remove(guest);
+        consoleLog(guest.username+" is removed from the room");
+
+        //해당 사용자가 나갔다고 방에 방송한다
+        //이미 목록에서 해당 사용자를 삭제했기 때문에, broacastToRoomExceptMe()를 사용하지 않아도 된다
+        broadcastToRoom(roomName, "out/"+guest.id+"/"+guest.username);
+    }
+
+    // /////////////////////////////////////////////
+
+
+    //사용자를 방의 손님목록에 추가하는 메소드
+    void addGuestToRoom(String roomName, Guest guest) {
+
+        //방 목록 맵에 저장되어 있던, 이 방의 손님 리스트를 참조한다
+        ArrayList<Guest> listOfGuestsInTheRoom_copied = roomHashMap.get(roomName);
+
+        //그 리스트에 손님을 추가한다
+        listOfGuestsInTheRoom_copied.add(guest);
+
+        //방 이름과 손님 수를 서버 콘솔에 출력한다
+        consoleLog("RoomName : " + roomName + ", Number of Guests : " + listOfGuestsInTheRoom_copied.size());
+    }
+
+
+    //새로운 방을 생성하는 메소드
+    void addRoom(String roomName, Guest guest) {
+
+        //새로운 손님 리스트를 생성
+        ArrayList<Guest> listOfGuestsOfNewRoom = new ArrayList<Guest>();
+
+        //이 리스트에 방을 만든 게스트를 추가한다
+        listOfGuestsOfNewRoom.add(guest);
+
+        //방 목록 hashmap에 이 방을 추가한다
+        roomHashMap.put(roomName, listOfGuestsOfNewRoom);
+
+        consoleLog("Name of the new Room :" + roomName);
+        consoleLog("Number of Guests(It must be '1') :" + listOfGuestsOfNewRoom.size());
+    }
+
+
+    //각 방의 인원을 반환하는 메소드
+    String getNumberOfGuestsInEachRoom(Set<String> roomList) throws Exception{
+
+        StringBuffer buffer_numberOfGuestsInEachRoom = new StringBuffer("roomnum/"); //방에 사람수
+
+        //각 방의 인원을 한 문장으로 나타냄
+        for(String roomName : roomList){
+            buffer_numberOfGuestsInEachRoom.append(roomHashMap.get(roomName).size()+"/");
+        }
+
+        return buffer_numberOfGuestsInEachRoom.toString();
+    }
+
+    /////////////////////////////////////////////////////////////
+
+
+    //특정 사용자를 대기실 손님 목록에서 삭제한다
+//    void removeGuestFromLobby(Guest guest) {
+//
+//        int number_before = listOfGuestsAtLobby.size();
+//        String guest_id = guest.id;
+//        String guest_username = guest.username;
+//
+//        listOfGuestsAtLobby.remove(guest);
+//
+//        int number_after = listOfGuestsAtLobby.size();
+//        consoleLog("Has removed guest "+guest_id+"/"+guest_username+". Number of Entire Guests : "+number_before+"->"+number_after);
+//    }
+
+
+    //대기실 접속자에게 메시지를 보내는 메소드
+//    void broadcastToEveryoneAtLobby(String msg) throws Exception {
+//
+//        try {
+//            for (Guest guest : listOfGuestsAtLobby) {
+//                guest.sendMsg(msg);
+//            }
+//
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//        }
+//
+//    }
+
+
+
+    //특정 방에 있는 사람들에게 메시지를 보낸다
+    void broadcastToRoom(String roomName, String msg) throws Exception {
+
+        ArrayList<Guest> listOfGuestsInTheRoom = roomHashMap.get(roomName);
+
+        for (Guest guest : listOfGuestsInTheRoom) {
+            guest.sendMsg(msg);
+        }
+    }
+
+
+    void broadcastToRoomExceptMe(String roomName, String msg, String userId) throws Exception {
+
+        ArrayList<Guest> listOfGuestsInTheRoom = roomHashMap.get(roomName);
+
+        for (Guest guest : listOfGuestsInTheRoom) {
+            if(!guest.id.equals(userId)){
+                guest.sendMsg(msg);
+            }
+        }
+    }
+
+
+    //대기실에 있는 모든 손님의 이름을 broadcast 한다
+    //guestlist/홍길동/김길동/이길동/ --이런식으로 클라이언트에 보내면,
+    //클라이언트가 '/' 단위로 쪼개서 화면에 출력할 것임
+//    void broadcastGuestlistAtLobby() throws Exception {
+//
+//        StringBuffer buffer = new StringBuffer("guestlist/"); //클라이언트에 보내는 신호
+//
+//        for (Guest guest : listOfGuestsAtLobby) {
+//            buffer.append(guest.id + "/");
+//        }
+//
+//        broadcastToEveryoneAtLobby(buffer.toString());
+//    }
+
+
+    //특정 방의 정보를 방송한다
+    //room_guestList/방이름/참여자 수/user1, user2, user3 -- 이런 식으로
+    void broadcastRoomInfo(String roomName) throws Exception {
+
+        ArrayList<Guest> guestListOfTheRoom = roomHashMap.get(roomName);
+
+        StringBuffer buffer_roomInfo = new StringBuffer("roomInfo/"+roomName+" ("+guestListOfTheRoom.size()+") ");
+
+        for (Guest guest : guestListOfTheRoom) {
+            buffer_roomInfo.append(guest.username + ", ");
+        }
+
+        broadcastToRoom(roomName, buffer_roomInfo.toString());
+    }
+
+
+    //서버 콘솔에 로그 남기기
+    public void consoleLog(String message){
+        System.out.println(message);
+    }
+
+
+
+
+    public static void main(String args[]) throws Exception {
+
+        Server server = new Server();
+
+        server.initNet();
+
+    }
+
+}
